ee093cf6f0a15971f5accecf1169cb679cc6d6fc
Ranjith <ransan95@gmail.com>
2019-10-20 11:36:19 +0530

    key change commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI: 'mongodb+srv://ranjith:ranjith@cluster0-h86bb.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://<username>:<password>@cluster0-h86bb.mongodb.net/test?retryWrites=true&w=majority'
 }
